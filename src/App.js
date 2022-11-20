83ab2936fddac76531f8aaef67f7f9ebc8764557
PriyaS1-cloud <118384937+PriyaS1-cloud@users.noreply.github.com>
2022-11-20 21:57:32 +0000

    importing React and adding css file in the app

--- src/App.js
+++ src/App.js
@@ -1,4 +1,8 @@
-// write hello-react in the div element which is having the id root, using React
-const h1 = React.createElement("h1", { id: 'main' }, "Hello React");
+// write hello-react by importing  React
+
+import React from "react";
+import  ReactDOM from "react-dom/client";
+
+const h1 = React.createElement("h1", { class: 'react-app' }, "Hello React using React");
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(h1);
