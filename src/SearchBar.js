6861896f0cde0a76965b2aba911f46e1a645a598
PriyaS1-cloud <118384937+PriyaS1-cloud@users.noreply.github.com>
2022-12-11 19:55:21 +0000

    fetching users from github api using useEffect

---
+++ src/SearchBar.js
@@ -0,0 +1,35 @@
+import { useState } from "react";
+import data from "./data.json";
+
+const SearchBar = ({ listofMembers, setFilteredListofMembers }) => {
+
+    const searchMembers = (text) => {
+        if(text.length==0){
+            return listofMembers;
+        }
+        return p = listofMembers.filter((data) => {
+            console.log(typeof text, text)
+            return data?.name.toLowerCase().includes(text.toLowerCase());
+        })
+
+
+    }
+
+    const [text, setText] = useState("");
+    return (<><div className="search">
+        <input className="search-input" value={text} onChange={
+            (e) => setText(e.target.value)
+        }
+        />
+        <button className="btn-search" onClick={() => {
+            filteredMember = searchMembers(text);
+            console.log(filteredMember);
+            setFilteredListofMembers(filteredMember)
+        }}>Search</button>
+
+    </div>
+        <div className="searchText">Searching For  {text} </div>
+    </>)
+
+}
+export default SearchBar;
